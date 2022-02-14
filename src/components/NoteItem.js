2a9c07f0cf5c0e1cd4064a8b268fb08590aca48c
Uddhav Patil <uddhavpatil9560@gmail.com>
2022-02-14 10:03:56 +0530

    upto 62 video

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const NoteItem = (props) => {
    const {note} = props;
   return (
@@ -8,11 +9,15 @@ const NoteItem = (props) => {
      
       <div class="card my-3">
        
-        <div class="card-body">
-            <h5 class="card-title">{note.title}</h5>
+        <div class="card-body align-items-center">
+           <div className="d-flex">
+           <h5 class="card-title">{note.title}</h5>
+            <i class="fa-solid fa-trash-can mx-2"></i>
+            <i class="fa-solid fa-pen-to-square mx-2"></i>
+            </div> 
             <p class="card-text"> {note.description}</p>
-        </div>
-      </div>
+            
+        </div></div>
     </div>
   )
 }
